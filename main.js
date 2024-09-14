a3a2bfb54f628a729287e0f6faea0c961cfad32a
AnasAhmedFND <anasahmed4402@gmail.com>
2024-09-14 10:05:12 +0600

    basic js

---
+++ main.js
@@ -0,0 +1,69 @@
+// one
+
+let sha = 25;
+let sba = 15;
+
+let total = sha + sba;
+console.log(total)
+
+// two
+let jontm = 500;
+let joncast = 200;
+
+let hm = jontm - joncast;
+
+console.log(hm)
+// three
+let everyhm = 120;
+let thour = 8;
+
+let et = everyhm * thour;
+
+console.log(et)
+// four/
+let sari = 12;
+let mutsari = 8;
+
+let ttree = sari * mutsari;
+let added = 20;
+console.log(ttree + added) 
+// five
+let rbh = 5;
+let everybox = 10;
+
+let tpic = rbh * everybox;
+let boxgive = 12;
+
+let result = tpic - boxgive;
+console.log(result)
+
+// six
+let trainrun = 150;
+let hour = 3;
+
+let bhagfhol = trainrun / hour;
+
+console.log(bhagfhol)
+
+// seven
+let masinsm = 200;
+let everym = 200 / 6;
+
+let addtwo = 2 + 6;
+let gun = addtwo * everym;
+
+let notsale = 3;
+
+let mibe = gun - notsale;
+console.log(mibe)
+
+// eight
+let chokletsope = 120;
+let everypek = 9;
+
+let pekatg = 13;
+let paketf = everypek * pekatg;
+
+let pkpimi = chokletsope - paketf;
+console.log(pkpimi)
+
