3097ecfbddb90ded2bc2164202734811d388556b
AnasAhmedFND <anasahmed4402@gmail.com>
2024-09-21 22:02:04 +0600

    array-q

---
+++ array-q.js
@@ -0,0 +1,25 @@
+// one
+let strings =['apple','banana','cherry'];
+console.log(strings)
+// two
+let numbers =[10,20,30,40,50,];
+console.log(numbers[2])
+// three
+let colors =['red','green','blue'];
+let added = colors.push('white'+'-'+'red');
+let addedTwo = colors.push('white'+'-'+'green');
+let addedthree = colors.push('white'+'-'+'blue');
+console.log(colors)
+// four
+let numberelement =[1,2,3,4,5];
+let max = numberelement.pop();
+console.log(numberelement)
+// five
+let fruits = ['apple','banana'];
+fruits.push('grape');
+console.log(fruits)
+// six
+let removeelement = ['car','bike','bus'];
+let remove = removeelement.shift();
+console.log(remove);
+console.log(removeelement)
---
+++ array.js
@@ -0,0 +1,20 @@
+let rollNumbers = [20,11,15,13,14,2,1,55,,8,74,66];
+
+console.log(rollNumbers[5]);
+console.log(rollNumbers);
+let run = rollNumbers.pop();
+console.log(rollNumbers);
+
+let max = rollNumbers.pop();
+console.log(rollNumbers);
+
+let names =['anik','Ronik','Riad','Ripa'];
+
+console.log(names[3]);
+names.push('anas');
+console.log(names);
+names.push('rohim','korim','abdulla');
+console.log(names)
+let nameed = names.reverse();
+console.log(nameed)
+
