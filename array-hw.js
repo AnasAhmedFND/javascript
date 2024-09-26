6ac8fa51ca037e66c31fb1efd629aa0eeeb14540
AnasAhmedFND <anasahmed4402@gmail.com>
2024-09-26 15:00:36 +0600

    array loop

---
+++ array-hw.js
@@ -0,0 +1,46 @@
+
+for (let i = 1; i<=100; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
+
+//    two
+
+for ( let i =0; i <=20; i++){
+    console.log(i);
+       
+}
+
+let bajur =[1,3,5,7,9,11,13,15,17,19];
+for (let i = 0; i <bajur.length; i++) {
+    console.log("Number is or - " + bajur[i]);
+}
+
+let jur =[2,4,6,8,10,12,14,16,18,20];
+for (let i = 0; i <jur.length; i++) {
+    console.log("Number is even - " + jur[i]);
+}
+
+// three
+let text =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+for (i = 0; i <text.length; i++){
+    if (i % 2 == 0) {
+        console.log("Number is or -- " + text[i]);
+    }
+}
+
+for (i = 0; i <text.length; i++) {
+    if (i % 2 !== 0) {
+        console.log("Number is even -- " + text[i]);
+    }
+}
+
+
+for (i = 0; i <text.length; i++) {
+    console.log(text[i] + 10);
+
+    
+}
+
+
