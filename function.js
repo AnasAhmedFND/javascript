d44e65fb09fa396815860da2093810a5189ef843
AnasAhmedFND <anasahmed4402@gmail.com>
2024-10-01 18:59:41 +0600

    function

--- function.js
+++ function.js
@@ -1,19 +1,17 @@
-// let array = [10, 20, 30, 40, 50];
-// let result = 0;
 
-// for (let i = 0; i <array.length; i++){
-//     result += array[i]
-    
-// }
-// console.log(result);
-
-let x = [10];
-let y = [20]
-
-function abc(x=10+10 ) {
+function abc(x=10+ 20+ 30+ 40+ 14+ 16+ 18+ 19+ 20+ 30 ) {
     console.log(x );
 
 }
 abc()
+
+
+
+function gpx(x=[10+ 20+ 30], y=[10+20]) {
+    console.log(x + y);
+
+}
+gpx(60, 30);
+gpx()
 
 
