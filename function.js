5b9b92054149e13022877f9045ac894f4a6ce018
AnasAhmedFND <anasahmed4402@gmail.com>
2024-10-03 11:47:28 +0600

    object

--- function.js
+++ function.js
@@ -1,112 +1,118 @@
-// No - 1 function dia array element (+)jug kora//
+// // No - 1 function dia array element (+)jug kora//
 
-let number = [10,20,30,40,50,60];
+// let number = [10,20,30,40,50,60];
 
-function evener(nums) {
-    let sum = 0;
-    for (i = 0; i <nums.length; i++){
-        sum += nums[i];
+// function evener(nums) {
+//     let sum = 0;
+//     for (i = 0; i <nums.length; i++){
+//         sum += nums[i];
 
-    }
-    return sum
+//     }
+//     return sum
 
-}
-console.log(evener(number))
-
-
-// NO - 2
-
-function abc (a) {
-    if (typeof (a) == "string"){
-        return "this is a string"
-
-    }else{
-        return "this is not a string"
-    }
-}
-console.log(abc("iii"));
+// }
+// console.log(evener(number))
 
 
-// ex:
-for ( i = 0; i <= 20; i++){
-    if(i % 2 == 0){
-        if(i % 2 == 0)
-        console.log("Number -" + i);
+// // NO - 2
 
-    }      
+// function abc (a) {
+//     if (typeof (a) == "string"){
+//         return "this is a string"
+
+//     }else{
+//         return "this is not a string"
+//     }
+// }
+// console.log(abc("iii"));
+
+
+// // ex:
+// for ( i = 0; i <= 20; i++){
+//     if(i % 2 == 0){
+//         if(i % 2 == 0)
+//         console.log("Number -" + i);
+
+//     }      
    
             
-}
+// }
 
 
 
-// some test items array method
-// lenght
-let xl = [ 20,22,0,10,33,44,5,7,5];
-let xlr = xl.length;
-console.log(xlr);
+// // some test items array method
+// // lenght
+// let xl = [ 20,22,0,10,33,44,5,7,5];
+// let xlr = xl.length;
+// console.log(xlr);
 
-// string
-let xs = ["anik","anas", "ripa"];
-let xsr = xs.toString();
-console.log(xsr);
+// // string
+// let xs = ["anik","anas", "ripa"];
+// let xsr = xs.toString();
+// console.log(xsr);
 
-// at
-let xa =[20,10,20,44,50];
-let xar =xa. at(2);
-console.log(xar)
+// // at
+// let xa =[20,10,20,44,50];
+// let xar =xa. at(2);
+// console.log(xar)
 
-// join
-let xj = [15,25,35,45];
-let xjr = xj.join("/");
-console.log(xjr);
+// // join
+// let xj = [15,25,35,45];
+// let xjr = xj.join("/");
+// console.log(xjr);
 
-// pop
-let xp = [11,22,33,44];
-let xpr = xp.pop();
-console.log(xp)
+// // pop
+// let xp = [11,22,33,44];
+// let xpr = xp.pop();
+// console.log(xp)
 
-// push
-let xpu = [10,20,30,40];
-xpu.push(50);
-console.log(xpu);
+// // push
+// let xpu = [10,20,30,40];
+// xpu.push(50);
+// console.log(xpu);
 
-// shift
-let xsh = [1,2,3,4];
-let xshr = xsh.shift();
-console.log(xsh);
+// // shift
+// let xsh = [1,2,3,4];
+// let xshr = xsh.shift();
+// console.log(xsh);
 
-// unshift
-let xuns = [13,23,33,43];
-let xunsr = xuns.unshift(3);
-console.log(xuns);
+// // unshift
+// let xuns = [13,23,33,43];
+// let xunsr = xuns.unshift(3);
+// console.log(xuns);
 
-// delete
-let xd = [50,100,150,200];
-delete xd [0];
-console.log(xd);
+// // delete
+// let xd = [50,100,150,200];
+// delete xd [0];
+// console.log(xd);
 
-// concat
-let xc = [0,1,2];
-let yc = [3,4];
-let xyr =xc.concat(yc);
-console.log(xyr);
+// // concat
+// let xc = [0,1,2];
+// let yc = [3,4];
+// let xyr =xc.concat(yc);
+// console.log(xyr);
 
-// flat
-let xf = [[1,2],[3,4],[5,6]];
-let xfr = xf.flat();
-console.log(xfr);
+// // flat
+// let xf = [[1,2],[3,4],[5,6]];
+// let xfr = xf.flat();
+// console.log(xfr);
 
-// splice
-let xsp = [7,6,5,4,3,2,1];
-let xspr = xsp.splice(1,3);
-console.log(xsp);
+// // splice
+// let xsp = [7,6,5,4,3,2,1];
+// let xspr = xsp.splice(1,3);
+// console.log(xsp);
 
-// slice
-let xsl = ["anik","anas","ripa","ashik"];
-let xslr = xsl.slice(1);
-console.log(xsl + " <br> <br> " + xslr);
+// // slice
+// let xsl = ["anik","anas","ripa","ashik"];
+// let xslr = xsl.slice(1);
+// console.log(xsl + " <br> <br> " + xslr);
 
 
+// Data Type Object//
 
+let person = {name:"Anik", age:19, bloodGroup:"A+", contact:""}
+person.height = "5pt";
+person.contact = 80724723
+console.log(person);
+console.log(person.height);
 
